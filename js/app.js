d19ab4f02aa84f46f745c02edecbc1fa3c5df4e5
joeyhofman123 <joeyhofman1@gmail.com>
2024-02-10 17:49:49 +0100

    changed from regular to arrow functions in app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { GDPR } from "./GDPR.js";
 import { ProfileAccordion } from "./profile_accordion.js";
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", (e) => {
     new GDPR();
     new ProfileAccordion();
 });
