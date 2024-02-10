e2ce42cec3f1efbb7e8001a5fcedb197ec6873ad
joeyhofman123 <joeyhofman1@gmail.com>
2024-02-10 15:35:03 +0100

    added initial accordion for skills

--- js/app.js
+++ js/app.js
@@ -2,3 +2,22 @@ import { GDPR } from "./GDPR.js";
 
 const gdpr = new GDPR();
 console.log(gdpr.getCookieStatus());
+
+document.addEventListener("DOMContentLoaded", function() {
+    var accordionTriggers = document.querySelectorAll(".profile-content-accordion__trigger");
+  
+    accordionTriggers.forEach((trigger) => {
+      trigger.addEventListener("click", (event) => {
+        const targetId = trigger.dataset.target;
+        const content = document.getElementById(targetId);
+        
+        if (content.style.height === '0px') {
+          content.style.height = content.scrollHeight + 'px';
+          trigger.setAttribute("aria-expanded", "true");
+        } else {
+            content.style.height = '0';
+            trigger.setAttribute("aria-expanded", "false");
+        }
+      });
+    });
+  });
